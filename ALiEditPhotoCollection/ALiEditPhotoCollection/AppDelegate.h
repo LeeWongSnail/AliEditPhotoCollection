fac7a94406768d245fdbc5084c0a46f38b8cf849
LeeWongSnail <@wangli_0632@163.com>
2016-09-28 18:46:39 +0800

    photo edit

---
+++ ALiEditPhotoCollection/ALiEditPhotoCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALiEditPhotoCollection
+//
+//  Created by LeeWong on 2016/9/28.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALiEditPhotoCollection/ALiEditPhotoCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiEditPhotoCollection
+//
+//  Created by LeeWong on 2016/9/28.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
